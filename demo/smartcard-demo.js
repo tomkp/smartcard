47b429c56918cd745341e18ebe184fee244ab3d7
tomkp <tom@tomkp.com>
2016-03-26 12:14:25 +0000

    ...

--- demo/smartcard-demo.js
+++ demo/smartcard-demo.js
@@ -6,15 +6,31 @@ const devices = new Devices();
 
 devices.on('device-activated', function (event) {
     const currentDevices = event.devices;
-    var device = event.device;
+    let device = event.device;
     console.log(`Device '${device}' activated, devices: ${currentDevices}`);
-    for (var prop in currentDevices) {
+    for (let prop in currentDevices) {
         console.log("Devices: " + currentDevices[prop]);
     }
 
     device.on('card-inserted', function (event) {
-        console.log(`Card '${event.card.atr()}' inserted into '${event.device}'`);
+        let card = event.card;
+        console.log(`Card '${card.atr()}' inserted into '${event.device}'`);
 
+        card.on('command-issued', function (event) {
+            console.log(`Command '${event.command}' issued to '${event.card}' `);
+        });
+
+        card.on('response-received', function (event) {
+            console.log(`Response '${event.response}' received from '${event.card}' in response to '${event.command}'`);
+        });
+
+        card
+            .issueCommand('00A404000E315041592E5359532E4444463031')
+            .then(function (response) {
+                console.log(`Response '${response.toString('hex')}`);
+            }).catch(function (error) {
+            console.error(error);
+        });
     });
     device.on('card-removed', function (event) {
         console.log(`Card removed from '${event.name}' `);
@@ -50,7 +66,7 @@ devices.on('card-inserted', function (event) {
 
     console.log(`List devices: ${devices.listDevices()}`);
 
-    var reader = event.reader;
+    let reader = event.reader;
     console.log(`Card inserted into '${reader.name}' `);
 
     devices
