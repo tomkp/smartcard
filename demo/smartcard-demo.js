af8da962a9d8be5e1e7892572795a3b88f4ee2d0
tomkp <tom@tomkp.com>
2016-03-26 21:44:30 +0000

    ...

--- demo/smartcard-demo.js
+++ demo/smartcard-demo.js
@@ -55,39 +55,3 @@ devices.on('device-deactivated', function (event) {
     console.log(`Device '${event.reader.name}' deactivated, devices: ${devices.listDevices()}`);
 });
 
-
-/*
-
-devices.on('device-deactivated', function (event) {
-    console.log(`Device '${event.reader.name}' deactivated, devices: ${devices.listDevices()}`);
-});
-
-
-devices.on('command-issued', function (event) {
-    console.log(`Command '${event.command}' issued to '${event.reader.name}' `);
-});
-
-devices.on('response-received', function (event) {
-    console.log(`Response '${event.response}' received from '${event.reader.name}' in response to '${event.command}'`);
-});
-
-devices.on('error', function (event) {
-    console.log(`Error '${event.error}' received`);
-});
-
-devices.on('card-inserted', function (event) {
-
-    console.log(`List devices: ${devices.listDevices()}`);
-
-    let reader = event.reader;
-    console.log(`Card inserted into '${reader.name}' `);
-
-    devices
-        .issueCommand(reader, '00A404000E315041592E5359532E4444463031')
-        .then(function (response) {
-            console.log(`Response '${response.toString('hex')}`);
-        }).catch(function (error) {
-            console.error(error);
-        });
-});
-*/
