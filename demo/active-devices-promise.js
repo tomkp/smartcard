02f56b5c30f3b877da9df2b92d6dd31cff771117
tomkp <tom@tomkp.com>
2016-05-11 21:47:50 +0100

    demo: active devices - promises

--- demo/active-devices-promise.js
+++ demo/active-devices-promise.js
@@ -6,10 +6,8 @@ const devices = new Devices();
 
 
 devices.onActivated().then(event => {
-    const currentDevices = event.devices;
-    let device = event.device;
-    console.log(`Device '${device}' activated, devices: ${currentDevices}`);
-    currentDevices.map((device, index) => {
+    console.log(`Device '${event.device}' activated`);
+    event.devices.map((device, index) => {
         console.log(`Device #${index + 1}: ${device.name}`);
     });
 });
