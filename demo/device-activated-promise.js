460a0eec5bc55f5182109562c08e7fef584339e9
tomkp <tom@tomkp.com>
2016-05-12 08:15:52 +0100

    ...

--- demo/device-activated-promise.js
+++ demo/device-activated-promise.js
@@ -8,6 +8,6 @@ const devices = new Devices();
 devices.onActivated().then(event => {
     console.log(`Device '${event.device}' activated`);
     event.devices.map((device, index) => {
-        console.log(`Device #${index + 1}: ${device.name}`);
+        console.log(`Device #${index + 1}: '${device.name}'`);
     });
 });
