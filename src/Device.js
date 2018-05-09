d3c84328e70b6b15e78dcfa67af35448444c09b0
flakjacket95 <dflack95@gmail.com>
2018-05-09 16:49:33 -0400

    Allow card sharing

--- src/Device.js
+++ src/Device.js
@@ -21,7 +21,7 @@ class Device extends EventEmitter {
         };
 
         const cardInserted = (reader, status) => {
-            reader.connect((err, protocol) => {
+            reader.connect({share_mode: 'SCARD_SHARE_SHARED'},(err, protocol) => {
                 if (err) {
                     this.emit('error', err);
                 } else {
