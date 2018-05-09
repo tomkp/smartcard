e9dc0dad9b42d2f88d0554e7e9fff910cf5ea670
flakjacket95 <dflack95@gmail.com>
2018-05-09 17:34:26 -0400

    Permenant fix for response size issues.

--- src/Iso7816Application.js
+++ src/Iso7816Application.js
@@ -85,7 +85,7 @@ class Iso7816Application extends EventEmitter {
 
     getResponse(length) {
 	      //When response is over 254 bytes long, I get buffer size errors
-        if(length > 0xfd || length == 0x00) length=0xfd;
+        //if(length > 0xfd || length == 0x00) length=0xfd;
         //console.log(`Iso7816Application.getResponse, length='${length}'`);
         return this.issueCommand(new CommandApdu({
             cla: 0x00,
