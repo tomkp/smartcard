8bf61220301f5184f8395ebbc1f1164c31c19e3f
tomkp <tom@tomkp.com>
2021-02-15 12:38:56 +0000

    Bump versions

--- demo/device-activated.js
+++ demo/device-activated.js
@@ -4,10 +4,9 @@ const smartcard = require('../lib/index');
 const Devices = smartcard.Devices;
 const devices = new Devices();
 
-
-devices.on('device-activated', event => {
-    console.log(`Device '${event.device}' activated`);
-    event.devices.map((device, index) => {
-        console.log(`Device #${index + 1}: '${device.name}'`);
-    });
+devices.on('device-activated', (event) => {
+  console.log(`Device '${event.device}' activated`);
+  event.devices.map((device, index) => {
+    console.log(`Device #${index + 1}: '${device.name}'`);
+  });
 });
