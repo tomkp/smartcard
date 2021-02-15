0cbfc19edc200e770a4d02edde4bf96749f1aacf
tomkp <tom@tomkp.com>
2021-02-15 12:38:56 +0000

    Bump versions

--- demo/device-activated-promise.js
+++ demo/device-activated-promise.js
@@ -4,10 +4,9 @@ const smartcard = require('../lib/index');
 const Devices = smartcard.Devices;
 const devices = new Devices();
 
-
-devices.onActivated().then(event => {
-    console.log(`Device '${event.device}' activated`);
-    event.devices.map((device, index) => {
-        console.log(`Device #${index + 1}: '${device.name}'`);
-    });
+devices.onActivated().then((event) => {
+  console.log(`Device '${event.device}' activated`);
+  event.devices.map((device, index) => {
+    console.log(`Device #${index + 1}: '${device.name}'`);
+  });
 });
--- demo/device-deactivated.js
+++ demo/device-deactivated.js
@@ -4,10 +4,9 @@ const smartcard = require('../lib/index');
 const Devices = smartcard.Devices;
 const devices = new Devices();
 
-
-devices.on('device-deactivated', event => {
-    console.log(`Device '${event.device}' deactivated`);
-    event.devices.map((device, index) => {
-        console.log(`Device #${index + 1}: ${device.name}`);
-    });
+devices.on('device-deactivated', (event) => {
+  console.log(`Device '${event.device}' deactivated`);
+  event.devices.map((device, index) => {
+    console.log(`Device #${index + 1}: ${device.name}`);
+  });
 });
