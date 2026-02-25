9610511d2f535e90277b1d56b23075d6f8c8d2c8
tomkp <tom@tomkp.com>
2026-02-25 14:02:05 +0000

    feat: implement DetectCardStateChange function

--- src/reader_state_utils.h
+++ src/reader_state_utils.h
@@ -1,2 +1,16 @@
 // src/reader_state_utils.h
 #pragma once
+#include <cstdint>
+
+enum class CardEvent { None, Inserted, Removed };
+
+constexpr uint32_t PCSC_STATE_PRESENT = 0x00000010;
+
+inline CardEvent DetectCardStateChange(uint32_t oldState, uint32_t newState) {
+    bool wasPresent = (oldState & PCSC_STATE_PRESENT) != 0;
+    bool isPresent = (newState & PCSC_STATE_PRESENT) != 0;
+
+    if (!wasPresent && isPresent) return CardEvent::Inserted;
+    if (wasPresent && !isPresent) return CardEvent::Removed;
+    return CardEvent::None;
+}
